02a3f5b739827cd4d26cb332466afbc5e3d3fe6e
Konrad Trybus <ktrybus15@gmail.com>
2025-08-18 14:36:46 +0200

    Login UI + simple function

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+
 app_name = 'account'
+
 urlpatterns = [
     path("dashboard/", views.dashboard, name='dashboard'),
+    path("login/", views.login_view, name='login'),
+    path("logout/", views.logout_view, name='logout'),
 ]
